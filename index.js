78a01c1a364672ccdf171fc13ad80f03dacb143d
donellgill <donell.gill@outlook.com>
2021-04-11 17:51:40 -0400

    got 1 test to pass still workin on other 2

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+
+let users = {
+    name: 'Steve',
+    email: 'steve@steve.com'
+}
+
+let configObj = {
+    method: 'Post',
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    },
+    body: JSON.stringify({users})
+    }
+
+function submitData(name, email){
+return fetch("http://localhost:3000/users", configObj)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    document.body.innerHTML = object[id];
+  })
+  .catch(function(error){
+      alert("Bad things! Ragnarok!");
+      document.body.innerHTML = error.message;
+    })
+};
